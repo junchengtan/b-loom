8f569469cbe6e042d06f50a9af64c3dcc5c552b7
jctan <jctan@umich.edu>
2020-08-31 01:20:24 +0800

    Shipping app for site

--- src/render.js
+++ src/render.js
@@ -52,8 +52,10 @@ async function selectSource(source) {
             chromeMediaSource: 'desktop',
             chromeMediaSourceId: source.id
         }
-        }
-  };
+   }
+ };
+ 
+  
   
     //create stream
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -92,3 +94,70 @@ async function handleStop(e) {
     console.log(filePath);
     writeFile(filePath, buffer, () => console.log('video saved successfully'));
 }
+
+
+// function startRecord() {
+//     electron.desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+//       if (error) throw error
+//       for (let i = 0; i < sources.length; ++i) {
+//         if (sources[i].name === "foo") {
+//           navigator.mediaDevices.getUserMedia({
+//             audio: false,
+//             video: {
+//               mandatory: {
+//                 chromeMediaSource: 'desktop',
+//                 chromeMediaSourceId: sources[i].id,
+//                 minWidth: 1280,
+//                 maxWidth: 1280,
+//                 minHeight: 720,
+//                 maxHeight: 720
+//               }
+//             }
+//           })
+//             .then((stream) => handleStream(stream))
+//             .catch((e) => handleError(e))
+//           return
+//         }
+//       }
+//     });
+// }
+
+// function handleStream(stream) {
+//     navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(mediaStream){
+//       var audioTracks = mediaStream.getAudioTracks();
+//       //add video and audio sound
+//       var medias = $("audio,video");
+//       for (var i = 0; i < medias.length; i++) {
+//         var tmpStream = medias[i].captureStream();  // mainWindow = new BrowserWindow({webPreferences: {experimentalFeatures: true} })
+//         if(tmpStream) {
+//           var tmpTrack = tmpStream.getAudioTracks()[0];
+//           audioTracks.push(tmpTrack);
+//         }
+//       }
+
+//       // mix audio tracks
+//       if(audioTracks.length > 0){
+//         var mixAudioTrack = mixTracks(audioTracks);
+//         stream.addTrack(mixAudioTrack);
+//       }
+
+//       stream.addTrack(audioTrack);
+//       recorder = new MediaRecorder(stream);
+//       recorder.ondataavailable = function(event) {
+//         // deal with your stream
+//       };
+//       recorder.start(1000);
+//     }).catch(function(err) {
+//       //console.log("handle stream error");
+//     })
+// }
+
+//   function mixTracks(tracks) {
+//     var ac = new AudioContext();
+//     var dest = ac.createMediaStreamDestination();
+//     for(var i=0;i<tracks.length;i++) {
+//       const source = ac.createMediaStreamSource(new MediaStream([tracks[i]]));
+//       source.connect(dest);
+//     }
+//     return dest.stream.getTracks()[0];
+//   }
